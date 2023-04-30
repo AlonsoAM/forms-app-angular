0997aeca27158325395bf05983e52ef026855bbb
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-30 18:10:27 -0500

    Validaciones básicas

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [],
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl('valor', [validaciones sincronas], [vcalidaciones asincronas]),
   //   price: new FormControl(0),
@@ -20,9 +20,41 @@ export class BasicPageComponent {
 
   constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    // this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Como mínimo debe tener ${errors['minlength'].requiredLength} letras`;
+      }
+    }
+
+    return null;
+  }
+
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
