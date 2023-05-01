9a8184ea03aa074d121364fd5787b6cefca57829
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-05-01 11:38:14 -0500

    Validaciones dinamicas terminadas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DynamicPageComponent {
+  public myform: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
 
+  public newFavorite: FormControl = new FormControl('', [Validators.required]);
+
+  constructor(private fb: FormBuilder) {}
+
+  get favoriteGames() {
+    return this.myform.get('favoriteGames') as FormArray;
+  }
+
+  onDeleteFavorite(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onAddFavorites(): void {
+    if (this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+
+    this.newFavorite.reset();
+  }
+
+  onSubmit(): void {
+    if (this.myform.invalid) {
+      this.myform.markAllAsTouched();
+      return;
+    }
+    console.log(this.myform.value);
+    (this.myform.controls['favoriteGames'] as FormArray) = this.fb.array([]);
+    this.myform.reset();
+  }
 }
