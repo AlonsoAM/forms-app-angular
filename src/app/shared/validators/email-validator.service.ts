524b34e14840f548e3ca34c114fe20921416273d
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-05-05 12:34:03 -0500

    Validador asincrono de email

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -8,11 +8,30 @@ import { Observable, delay, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidatorService implements AsyncValidator {
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email });
+  //   return of({
+  //     emailTaken: true,
+  //   }).pipe(delay(2000));
+  // }
+
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({ email });
-    return of({
-      emailTaken: true,
-    }).pipe(delay(2000));
+
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        console.log({ email });
+        if (email === 'alonso.amoreno@live.com') {
+          subscriber.next({ emailTaken: true });
+          subscriber.complete();
+          // return;
+        }
+        subscriber.next(null);
+        subscriber.complete();
+      }
+    ).pipe(delay(3000));
+
+    return httpCallObservable;
   }
 }
